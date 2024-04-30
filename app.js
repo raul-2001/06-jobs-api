8f6a678fa5db2e3a7fb5016f22c1c75cd6aebe73
raul-2001 <rlqv2001@gmail.com>
2024-04-30 12:47:59 -0400

    updated on app.js

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ const itemRouter = require('./routes/items')
 const orderRouter = require('./routes/orders')
 
 
+
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 app.use(rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100,
@@ -34,6 +36,9 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get('/', (req, res) => {
+  res.send("Order api")
+})
 
 // routes
 app.use('/api/v1/auth', authRouter)
