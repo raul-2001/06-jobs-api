368f6098032289b7f8d4ea4080caf9a5c414aeb1
raul-2001 <rlqv2001@gmail.com>
2024-05-01 15:37:08 -0400

    added swagger.yaml

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 //connection
 const connectDB = require('./db/connect')
@@ -37,8 +41,10 @@ app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send("Order api")
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation<a/>')
 })
+
+app.use('api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
